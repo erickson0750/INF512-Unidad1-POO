eb3770afe0b6c4aad6ec1f22a921749bc08c807e
erickson0750 <164575812+erickson0750@users.noreply.github.com>
2026-02-05 10:36:25 -0400

    Se agrega clase Persona con atributos

---
+++ Persona.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Persona
+{
+    public string Nombre;
+    public int Edad;
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine("Nombre: " + Nombre);
+        Console.WriteLine("Edad: " + Edad);
+    }
+}
